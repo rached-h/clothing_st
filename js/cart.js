0212dda7c1333bd2dfdcc5c14ec60d8e72cb67da
rached hammouche <rachedhammouche5@gmail.com>
2025-03-15 16:48:14 +0100

    +checkout

--- js/cart.js
+++ js/cart.js
@@ -4,3 +4,24 @@ all_added.forEach(prc => {
     total += parseFloat(prc.innerHTML);
 })
 document.getElementById("total").innerHTML=total;
+
+
+function deliveryType(type) {
+    document.getElementById('city-commune').classList.add('disactivated');
+    document.getElementById('address').classList.add('disactivated');
+    document.getElementById(type).classList.remove('disactivated');
+}
+
+let popup = document.getElementById("pop-up");
+let checkout = document.getElementById("checkout");
+let close = document.getElementById("close");
+let overlay = document.getElementById("overlay");
+checkout.addEventListener('click',() => {popup.classList.add('active');
+overlay.style.display="block";
+overlay.style.opacity="1";
+});
+close.addEventListener('click',() => {popup.classList.remove('active');
+    overlay.style.display="none";
+    overlay.style.opacity="0";
+});
+
